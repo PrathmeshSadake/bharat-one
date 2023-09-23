eda64d9a08b987980ad9e734210d377757269e2c
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-09-23 19:24:12 +0530

    Added users table

--- src/pages/add-user.jsx
+++ src/pages/add-user.jsx
@@ -1,15 +1,33 @@
 import { collection, addDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../lib/firebase";
 import toast from "react-hot-toast";
+import { useState } from "react";
 
 const AddUserPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [city, setCity] = useState("");
+
   const addUser = async (e) => {
     e.preventDefault();
+    if (
+      name.length === 0 ||
+      email.length === 0 ||
+      age.length === 0 ||
+      gender.length === 0 ||
+      city.length === 0
+    ) {
+      return toast.error("Please enter all required fields");
+    }
     try {
       const docRef = await addDoc(collection(db, "users"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815,
+        name,
+        email,
+        age,
+        gender,
+        city,
       });
       console.log("Document written with ID: ", docRef.id);
       const userRef = doc(db, "users", docRef.id);
@@ -18,67 +36,83 @@ const AddUserPage = () => {
       });
 
       toast.success("User added successfully.");
+      setName("");
+      setEmail("");
+      setAge("");
+      setCity("");
+      setGender("");
+      window.location.replace("/");
     } catch (e) {
-      console.error("Error adding document: ", e);
+      toast.error("Error adding document");
     }
   };
   return (
-    <div className='mx-auto max-w-7xl py-12 min-h-screen w-full'>
+    <div className='mx-auto max-w-7xl py-24 min-h-screen w-full'>
       <div className='w-full mx-auto max-w-md'>
         <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2'>
-              Username
+              Name
             </label>
             <input
               className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              id='username'
+              id='name'
               type='text'
-              placeholder='Username'
+              placeholder='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2'>
-              Username
+              Email
             </label>
             <input
               className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              id='username'
+              id='email'
               type='text'
-              placeholder='Username'
+              placeholder='you@domain.com'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2'>
-              Username
+              Age
             </label>
             <input
               className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              id='username'
+              id='age'
               type='text'
-              placeholder='Username'
+              placeholder='Age'
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
             />
           </div>
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2'>
-              Username
+              Gender
             </label>
             <input
               className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              id='username'
+              id='gender'
               type='text'
-              placeholder='Username'
+              placeholder='Gender'
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
             />
           </div>
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2'>
-              Username
+              City
             </label>
             <input
               className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              id='username'
+              id='city'
               type='text'
-              placeholder='Username'
+              placeholder='City'
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
             />
           </div>
 
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,29 +1,67 @@
-import { useCollection } from "react-firebase-hooks/firestore";
+import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
 import { db } from "../lib/firebase";
 import { collection } from "firebase/firestore";
-import React from "react";
 
 const HomePage = () => {
-  const [value, loading, error] = useCollection(collection(db, "users"), {
-    snapshotListenOptions: { includeMetadataChanges: true },
-  });
-  console.log(value);
+  const [values, loading, error] = useCollectionDataOnce(
+    collection(db, "users"),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
+  if (values) {
+    console.log(values);
+  }
+
   return (
-    <div>
+    <div className='mx-auto max-w-7xl py-24'>
       <p>
         {error && <strong>Error: {JSON.stringify(error)}</strong>}
         {loading && <span>Collection: Loading...</span>}
-        {value && (
-          <span>
-            Collection:{" "}
-            {value.docs.map((doc) => (
-              <React.Fragment key={doc.id}>
-                {JSON.stringify(doc.data())},{" "}
-              </React.Fragment>
-            ))}
-          </span>
-        )}
       </p>
+      {values && (
+        <div className='relative overflow-x-auto'>
+          <table className='w-full text-sm text-left text-gray-500'>
+            <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
+              <tr>
+                <th scope='col' className='px-6 py-3'>
+                  Name
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Email
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Age
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Gender
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  City
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {values.map((user) => {
+                return (
+                  <tr key={user.id} className='bg-white border-b'>
+                    <th
+                      scope='row'
+                      className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'
+                    >
+                      {user.name}
+                    </th>
+                    <td className='px-6 py-4'> {user.email}</td>
+                    <td className='px-6 py-4'> {user.age}</td>
+                    <td className='px-6 py-4'> {user.gender}</td>
+                    <td className='px-6 py-4'> {user.city}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
